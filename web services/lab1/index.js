1778f8e3eb40fa95e844bce0121046a196d795cd
Islam Sulaiman <ma.eslam@yahoo.com>
2023-05-25 13:04:55 +0300

    web services labs

--- web services/lab1/index.js
+++ web services/lab1/index.js
@@ -17,14 +17,16 @@ const scope = 'user repo'
 
 // helpers
 const fetchUserProfile = async (accessToken) => {
-    return axios({
+    const {data} = await axios({
         method: 'get',
         url: 'https://api.github.com/user',
         headers: {
             Authorization: `token ${accessToken}`
         }
     })
+    return data
 }
+
 
 
 // Routes
@@ -54,15 +56,189 @@ app.get('/home', async function (req, res) {
         }
     });
 
-    console.log(tokenResponse.data)
-
     const accessToken = tokenResponse.data.access_token;
 
     const sessid = crypto.randomBytes(16).toString('base64');
     authorizedUsers[sessid] = accessToken;
 
+    const data = await fetchUserProfile(accessToken)
+
+    const user = {
+            "userName ": data.login,
+            "ID" : data.id
+        }
+
     res.setHeader('Set-Cookie', `sessid=${sessid}`);
-    res.json('success');
+    res.json(user);
+});
+
+
+app.get('/profile', async function (req, res, next) {
+    const cookie = req.headers.cookie;
+    const sessid = cookie.replace('sessid=', '');
+    const accessToken = authorizedUsers[sessid];
+    let data;
+    try {
+        const response = await fetchUserProfile(accessToken);
+        data = response;
+        console.log(data)
+
+        const profileURL = await axios({
+            method: 'get',
+            headers: {
+                accept: 'application/json'
+            },
+            url: data.url,
+            params: {
+                client_id: clientId, // application
+                client_secret: clientSecret, // application
+            }
+        });
+        // const user = {
+        //     "userName ": data.login,
+        //     "ID" : data.id
+        // }
+        data = profileURL.data;
+        
+        res.json(data);
+    } catch (err) {
+        // next(err);
+        console.log(err)
+    } 
+});
+
+
+
+
+app.get('/followers', async function (req, res, next) {
+    const cookie = req.headers.cookie;
+    const sessid = cookie.replace('sessid=', '');
+    const accessToken = authorizedUsers[sessid];
+    let data;
+    try {
+        const response = await fetchUserProfile(accessToken);
+        data = response;
+        console.log(data)
+
+        const profileURL = await axios({
+            method: 'get',
+            headers: {
+                accept: 'application/json'
+            },
+            url: data.followers_url,
+            params: {
+                client_id: clientId, // application
+                client_secret: clientSecret, // application
+            }
+        });
+        // const user = {
+        //     "userName ": data.login,
+        //     "ID" : data.id
+        // }
+        data = profileURL.data;
+        
+        res.json(data);
+    } catch (err) {
+        // next(err);
+        console.log(err)
+    } 
+});
+
+
+
+// app.get('/followers', async function (req, res, next) {
+//     const cookie = req.headers.cookie;
+//     const sessid = cookie.replace('sessid=', '');
+//     const accessToken = authorizedUsers[sessid];
+//     let data;
+//     try {
+//         const response = await fetchUserProfile(accessToken);
+//         data = response.data;
+//         console.log(response)
+//         const user = {
+//             "userName ": data.login,
+//             "ID" : data.id
+//         }
+//         res.json(user);
+//     } catch (err) {
+//         // next(err);
+//         console.log(err)
+//     } 
+// });
+
+
+
+
+
+// app.get('/profile', async function (req, res, next) {
+
+//     let data;
+//     try {
+//         const response = await fetchUserProfile(globalAccessToken);
+//         data = response.data;
+
+//         const profileURL = await axios({
+//             method: 'get',
+//             headers: {
+//                 accept: 'application/json'
+//             },
+//             url: data.url,
+//             params: {
+//                 client_id: clientId, 
+//                 client_secret: clientSecret, 
+//             }
+//         });
+//         data = profileURL.data;
+//         res.json(data);
+//     } catch (err) {
+//         // next(err);
+//         res.json(err);
+//     }
+//     // res.json(data);
+// });
+
+
+app.get('/repos', async function (req, res, next) {
+    const cookie = req.headers.cookie;
+    const sessid = cookie.replace('sessid=', '');
+    const accessToken = authorizedUsers[sessid];
+    let data;
+    try {
+        const response = await fetchUserProfile(accessToken);
+        data = response;
+        console.log(data)
+
+        const profileURL = await axios({
+            method: 'get',
+            headers: {
+                accept: 'application/json'
+            },
+            url: data.repos_url,
+            params: {
+                client_id: clientId, // application
+                client_secret: clientSecret, // application
+            }
+        });
+        // const user = {
+        //     "userName ": data.login,
+        //     "ID" : data.id
+        // }
+        data = profileURL.data;
+        
+        res.json(data);
+    } catch (err) {
+        // next(err);
+        console.log(err)
+    } 
+});
+
+
+app.use((error, req, res, next) => {
+    if (error.response.data.message === 'Bad credentials') return res.json({
+        code: 'UNAUTHORIZED',
+        message: 'Please login again'
+    });
+    res.json(error.response.data);
 });
 
 
--- web services/lab1/try.js
+++ web services/lab1/try.js
@@ -1,82 +1,142 @@
 const express = require('express');
 const axios = require('axios');
 const crypto = require('crypto');
-
+const client_id = '2c8626622e0d9b3c45d0';
+const client_secret = 'e107b9d538857918c779d0197a858589a9c7cad2';
+const redirect_uri='http://localhost:3000/redirect'
 const app = express();
+const scope = 'user';
+const authorizedUsers={};
+app.listen(3000,()=>{
+    console.log('Start 3000');
+})
 
-const authorizedUsers = {  };
 
-const clientId = "XXXXXXX";
-const clientSecret = "XXXXXXXXXXXXXXXXXXX";
-
-const appRedirectUri = "http://localhost:3000/redirect"
-const scope = "user repo";
-
-app.listen(3000, function () {
-    console.log('Server started on port: ', this.address().port);
+app.get('/',async (req,res,next)=>{
+//     await axios({
+//         method:'get',
+//         url:`https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`,
+//         headers:{
+//             accept:'application/json'
+//         }
+//    });
+    //  res.json({
+    //     loginByGitHub : `GET https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`
+    //  });
+     res.redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`)
 });
 
-app.get('/', function (req, res) {
-    res.json({
-        loginByGitHub: `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${appRedirectUri}&scope=${scope}`
-    });
-});
-
-
-const fetchUserProfile = async (accessToken) => {
-    return axios({
-        method: 'get',
-        url: 'https://api.github.com/user',
-        headers: {
-            Authorization: `token ${accessToken}`
-        }
-    })
-}
-
-app.get('/redirect', async function (req, res) {
-    const { code } = req.query;
-
-    const tokenResponse = await axios({
-        method: 'post',
-        headers: {
-            accept: 'application/json'
+app.get('/redirect',async (req,res,next)=>{
+    const { code } = req.query
+   const response = await axios({
+        method:'post',
+        url:'https://github.com/login/oauth/access_token',
+        headers:{
+            accept:'application/json'
         },
-        url: 'https://github.com/login/oauth/access_token',
-        params: {
-            client_id: clientId, // application
-            client_secret: clientSecret, // application
-            code, // user
+        params:{
+            client_id,
+            client_secret,
+            code
         }
-    });
+   });
+   
+   const access_token = response.data.access_token;
+   const data = await getGitHubUserData(access_token);
+   const sessid = crypto.randomBytes(16).toString('base64');
+   authorizedUsers[sessid] = access_token;
 
-    const accessToken = tokenResponse.data.access_token;
-
-    const sessid = crypto.randomBytes(16).toString('base64');
-    authorizedUsers[sessid] = accessToken;
-
-    res.setHeader('Set-Cookie', `sessid=${sessid}`);
-    res.json('success');
+   res.setHeader('Set-Cookie', `sessid=${sessid}`);
+   res.json(data)
 });
 
-app.get('/profile', async function (req, res, next) {
+async function getGitHubUserData(access_token) {
+    const { data } = await axios({
+      url: 'https://api.github.com/user',
+      method: 'get',
+      headers: {
+        Authorization: `token ${access_token}`,
+      },
+    });
+    return data;
+  };
+
+  app.get('/profile', async function (req, res, next) {
     const cookie = req.headers.cookie;
     const sessid = cookie.replace('sessid=', '');
     const accessToken = authorizedUsers[sessid];
-    let data;
     try {
-        const response = await fetchUserProfile(accessToken);
-        data = response.data;
+        const response = await getGitHubUserData(accessToken);
+        let data = response;
+        console.log(data);
+        const profileURL = await axios({
+            method: 'get',
+            headers: {
+                accept: 'application/json'
+            },
+            url: data.url,
+            params: {
+              client_id,
+              client_secret
+            }
+        });
+        data = profileURL.data;
+        res.json(data);
     } catch (err) {
-        next(err);
+        res.json(err);
     }
-    res.json();
 });
 
+app.get('/followers', async function (req, res, next) {
+  const cookie = req.headers.cookie;
+  const sessid = cookie.replace('sessid=', '');
+  const accessToken = authorizedUsers[sessid];
+  let data;
+  try {
+      const response = await getGitHubUserData(accessToken);
+      data = response;
 
-app.use((error, req, res, next) => {
-    if (error.response.data.message === 'Bad credentials') return res.json({
-        code: 'UNAUTHORIZED',
-        message: 'Please login again'
-    });
-    res.json(error.response.data);
+      const profileURL = await axios({
+          method: 'get',
+          headers: {
+              accept: 'application/json'
+          },
+          url: data.followers_url,
+          params: {
+            client_id,
+            client_secret
+          }
+      });
+      data = profileURL.data;
+      res.json(data);
+  } catch (err) {
+      res.json(err);
+  }
+});
+
+app.get('/repos', async function (req, res, next) {
+  const cookie = req.headers.cookie;
+  const sessid = cookie.replace('sessid=', '');
+  const accessToken = authorizedUsers[sessid];
+  let data;
+  try {
+      const response = await getGitHubUserData(accessToken);
+      data = response;
+
+      const profileURL = await axios({
+          method: 'get',
+          headers: {
+              accept: 'application/json'
+          },
+          url: data.repos_url,
+          params: {
+            client_id,
+            client_secret
+          }
+      });
+      data = profileURL.data;
+      res.json(data);
+  } catch (err) {
+      res.json(err);
+  }
 });
